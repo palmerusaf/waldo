219f02d3182cfafc28466748f1aaf54d1a7296f9
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-12 21:20:19 +0700

    create main screen

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <>
-      <h1>main screen</h1>
-      <Link to="/game">Game Screen</Link>
-      <br />
-      <Link to="/highscores">High scores</Link>
+      <h1>Where's Waldo</h1>
+      <h2>Directions</h2>
+      <p>
+        Find all the characters in the picture, then check the high score page
+        to see where you rank up.
+      </p>
+      <ul>
+        <li>
+          <Link to="/game">Start Game</Link>
+        </li>
+        <li>
+          <Link to="/highscores">High Scores</Link>
+        </li>
+      </ul>
     </>
   );
 }
