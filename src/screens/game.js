6a07c38c28aa845257acdf391e783cc5eb6b1a5a
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-14 16:58:44 +0700

    add character imgs

--- src/screens/game.js
+++ src/screens/game.js
@@ -1,3 +1,8 @@
+import Canvas from '../imgs/waldo-canvas.jpg';
 export default function Game(props) {
-  return <h1>game screen</h1>;
+  return (
+    <div className="screen-container">
+      <img src={Canvas} alt="Waldo Canvas" />
+    </div>
+  );
 }
