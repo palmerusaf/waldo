5b6fd64c812838fbc664cf50ee64fd2e9417b3f9
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-16 18:14:15 +0700

    refactor GameCanvas

---
+++ src/components/GameCanvas.js
@@ -0,0 +1,5 @@
+import Canvas from "../imgs/waldo-canvas.jpg";
+
+export default function GameCanvas() {
+  return <img src={Canvas} alt="Waldo Canvas" />;
+}
--- src/screens/game.js
+++ src/screens/game.js
@@ -1,12 +1,12 @@
-import Canvas from "../imgs/waldo-canvas.jpg";
 import CharacterBar from "../components/CharacterBar.js";
+import GameCanvas from "../components/GameCanvas.js";
 import { Link } from "react-router-dom";
 
-export default function Game(props) {
+export default function Game() {
   return (
     <div className="screen-container">
       <CharacterBar />
-      <img src={Canvas} alt="Waldo Canvas" />
+      <GameCanvas />
       <Link to="/highscores">High Scores</Link>
     </div>
   );
