f0ba1095e94ffa3efe3bae8b2b502a6300a15856
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-16 19:10:21 +0700

    add state for chars

--- src/screens/game.js
+++ src/screens/game.js
@@ -1,8 +1,17 @@
 import CharacterBar from "../components/CharacterBar.js";
 import GameCanvas from "../components/GameCanvas.js";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 export default function Game() {
+  const [charStatus, setCharStatus] = useState([
+    { name: "waldo", found: false },
+    { name: "winda", found: false },
+    { name: "wizard", found: false },
+    { name: "woof", found: false },
+    { name: "odlaw", found: false },
+  ]);
+
   return (
     <div className="screen-container">
       <CharacterBar />
