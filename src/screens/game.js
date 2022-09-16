956f65b7df850c655e17232be4967dab2e9ea11f
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-16 18:02:47 +0700

    add link to high scores on game screen

--- src/screens/game.js
+++ src/screens/game.js
@@ -1,11 +1,13 @@
 import Canvas from "../imgs/waldo-canvas.jpg";
 import CharacterBar from "../components/CharacterBar.js";
+import { Link } from "react-router-dom";
 
 export default function Game(props) {
   return (
     <div className="screen-container">
       <CharacterBar />
       <img src={Canvas} alt="Waldo Canvas" />
+      <Link to="/highscores">High Scores</Link>
     </div>
   );
 }
