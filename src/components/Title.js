c23c03d7680866702572a78650ebf4052fcd4778
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-20 15:34:19 +0700

    style Title

---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+export default function Title(props) {
+  return (
+    <div className="font-mono w-fit p-1 px-4 text-center -rotate-12 mt-5 font-extrabold text-4xl bg-white">
+      <h1 className="text-blue-500">WHERE'S</h1>
+      <h1 className="text-red-600 outline-1">WALDO</h1>
+    </div>
+  );
+}
