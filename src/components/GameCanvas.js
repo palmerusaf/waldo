416b5063edbb145c749cff0a97b0bc0f18c38bf6
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-22 18:29:50 +0700

    display targetingbox when canvas is focused

--- src/components/GameCanvas.js
+++ src/components/GameCanvas.js
@@ -1,16 +1,35 @@
 import Canvas from "../imgs/waldo-canvas.jpg";
 import { getRelativeClickPosition } from "../utils/TargetingUtils.js";
 import TargetingDisplay from "./TargetingDisplay";
+import React, { useState } from "react";
 
 export default function GameCanvas() {
   const handleClick = (click) => {
-    console.log(getRelativeClickPosition(click));
+    const { x, y } = getRelativeClickPosition(click);
+    setCoordinates({ x, y });
   };
 
+  const [isFocused, setIsFocused] = useState(false);
+
+  const toggleIsFocused = () => {
+    setIsFocused(!isFocused);
+  };
+
+  const [coordinates, setCoordinates] = useState({ x: null, y: null });
+
   return (
-    <div className="flex relative" onClick={handleClick}>
-      <img onClick={handleClick} src={Canvas} alt="Waldo Canvas" />
-      <TargetingDisplay></TargetingDisplay>
-    </div>
+    <>
+      <button
+        className="flex relative"
+        onFocus={toggleIsFocused}
+        onBlur={toggleIsFocused}
+        onClick={handleClick}
+      >
+        <img src={Canvas} alt="Waldo Canvas" />
+        {isFocused && <TargetingDisplay />}
+      </button>
+      <div className="text-white">x: {coordinates.x}</div>
+      <div className="text-white">y: {coordinates.y}</div>
+    </>
   );
 }
--- src/components/TargetList.js
+++ src/components/TargetList.js
@@ -2,6 +2,11 @@ import { useCharacterContext } from "../contexts/CharacterContext";
 import CharacterImg from "../components/CharacterImg.js";
 import styled from "styled-components";
 
+const LI = styled.li`
+  background-color: rgba(156, 163, 175, 0.6);
+  border-width: 1px;
+`;
+
 export default function TargetList({ className }) {
   const { characterList, setCharacterFound } = useCharacterContext();
 
@@ -10,11 +15,6 @@ export default function TargetList({ className }) {
       console.log("TODO check database for", name, "positioning.");
       setCharacterFound(name);
     };
-
-    const LI = styled.li`
-      background-color: rgba(156, 163, 175, 0.6);
-      border-width: 1px;
-    `;
 
     return (
       !isFound && (
