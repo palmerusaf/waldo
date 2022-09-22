e4110370f5d40f9746848321db85c789ecc1dde0
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-22 16:31:53 +0700

    add magnifying glass

--- src/components/TargetList.js
+++ src/components/TargetList.js
@@ -1,7 +1,7 @@
 import { useCharacterContext } from "../contexts/CharacterContext";
 import CharacterImg from "../components/CharacterImg.js";
 
-export default function TargetList() {
+export default function TargetList({ className }) {
   const { characterList, setCharacterFound } = useCharacterContext();
 
   const TargetItem = ({ name, isFound }) => {
@@ -15,7 +15,7 @@ export default function TargetList() {
         <li
           onClick={handleClick}
           key={name}
-          className="flex m-px bg-gray-400 py-0.5 rounded-full"
+          className="grid content-center border-solid border-red-600 border-2 m-px bg-gray-400 py-0.5 rounded-full"
         >
           <CharacterImg name={name} />
         </li>
@@ -24,7 +24,7 @@ export default function TargetList() {
   };
 
   return (
-    <ul className="grid grid-cols-5 max-h-full">
+    <ul className={"grid grid-cols-5 max-h-full " + className}>
       {characterList.map(TargetItem)}
     </ul>
   );
--- src/components/TargetingDisplay.js
+++ src/components/TargetingDisplay.js
@@ -1,21 +1,20 @@
 import styled from "styled-components";
 import TargetList from "./TargetList";
+import MagGlass from "../imgs/mag-glass.png";
 
 const TargetingContainer = styled.div`
   position: absolute;
-  height: 25%;
-  width: 25%;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
   top: ${(props) => props.x}%;
   left: ${(props) => props.y}%;
 `;
 
 export default function TargetingDisplay(props) {
   return (
-    <TargetingContainer
-      x={props.x}
-      y={props.y}
-    >
-      <TargetList />
+    <TargetingContainer x={props.x} y={props.y} className="h-1/5 w-1/3 ">
+      <img src={MagGlass} alt="Magnifying Glass" className="h-full" />
+      <TargetList className="" />
     </TargetingContainer>
   );
 }
