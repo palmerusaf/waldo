617ee9b583ca262b4d117415b114f3abdaece0f2
Branden Palmer <brandenpalmer08@gmail.com>
2022-09-23 22:01:36 +0700

    fix targetingbox in wrong pos when page scrolled

--- src/utils/TargetingUtils.js
+++ src/utils/TargetingUtils.js
@@ -2,8 +2,8 @@ const formatAsInteger = (percent) => Math.floor(percent * 100);
 
 export const getRelativeClickPosition = (click) => {
   const target = document.getElementById("canvas");
-  const inTargetX = click.clientX - target.offsetLeft;
-  const inTargetY = click.clientY - target.offsetTop;
+  const inTargetX = click.pageX - target.offsetLeft;
+  const inTargetY = click.pageY - target.offsetTop;
 
   return {
     x: formatAsInteger(inTargetX / target.clientWidth),
