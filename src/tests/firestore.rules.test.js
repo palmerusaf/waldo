87f048462b267f4b55aba75b5050d5eaa496cc3d
Branden Palmer <brandenpalmer08@gmail.com>
2022-10-04 16:53:36 +0700

    add characterLocations tests

--- src/tests/firestore.rules.test.js
+++ src/tests/firestore.rules.test.js
@@ -7,14 +7,22 @@ import {
   initializeTestEnvironment,
   RulesTestEnvironment,
 } from "@firebase/rules-unit-testing";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+
 const fs = require("fs");
 
 const MY_PROJECT_ID = "waldo-7d581";
 
 const getTestEnv = async () =>
   initializeTestEnvironment({
-    projectId: "waldo-7d581",
+    projectId: MY_PROJECT_ID,
     firestore: {
       host: "localhost",
       port: "8080",
@@ -27,9 +35,15 @@ const getFirestore = async () => {
   return testEnv.unauthenticatedContext().firestore();
 };
 
-const getApp = async () => {
-  const testEnv = await getTestEnv();
-  return testEnv.unauthenticatedContext();
+const addTestLocation = async (testEnv) => {
+  const db = testEnv.unauthenticatedContext().firestore();
+  const locationsCol = collection(db, "characterLocations");
+  const location = { name: "waldo", minX: 20, maxX: 30, minY: 20, maxY: 30 };
+  //write test doc
+  const docId = await testEnv.withSecurityRulesDisabled(
+    addDoc(locationsCol, location)
+  );
+  return docId;
 };
 
 const getRankings = async (db) => {
@@ -61,8 +75,40 @@ const getCharacterLocations = async (db) => {
 };
 
 describe("characterLocations Tests", () => {
-  it("can read items in read-only collection", async () => {
+  it("can read in list mode", async () => {
     const db = await getFirestore();
+
     await assertSucceeds(getCharacterLocations(db));
+  });
+
+  it("cannot read single docs", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+    const docId = await addTestLocation(testEnv);
+
+    await assertFails(db.collection("characterLocations").doc(docId).get());
+  });
+
+  it("cannot delete single docs", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+    const docId = await addTestLocation(testEnv);
+
+    await assertFails(db.collection("characterLocations").doc(docId).delete);
+  });
+
+  it("cannot delete collection", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+    const docId = await addTestLocation(testEnv);
+
+    await assertFails(deleteDoc(doc(db, "characterLocations")));
+  });
+
+  it("cannot update docs", async () => {
+    const testEnv = await getTestEnv();
+    const docId = await addTestLocation(testEnv);
+
+    await assertFails(updateDoc(docId, { name: "odlaw" }));
   });
 });
