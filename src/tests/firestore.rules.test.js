a65ff1f9fa3ae29fbb3b898915bad555dff1e5c2
Branden Palmer <brandenpalmer08@gmail.com>
2022-10-04 19:03:22 +0700

    add tests for firestore.rules

--- src/tests/firestore.rules.test.js
+++ src/tests/firestore.rules.test.js
@@ -21,7 +21,7 @@ const fs = require("fs");
 const MY_PROJECT_ID = "waldo-7d581";
 
 const getTestEnv = async () =>
-  initializeTestEnvironment({
+  await initializeTestEnvironment({
     projectId: MY_PROJECT_ID,
     firestore: {
       host: "localhost",
@@ -35,16 +35,29 @@ const getFirestore = async () => {
   return testEnv.unauthenticatedContext().firestore();
 };
 
-const addTestLocation = async (testEnv) => {
-  const db = testEnv.unauthenticatedContext().firestore();
-  const locationsCol = collection(db, "characterLocations");
-  const location = { name: "waldo", minX: 20, maxX: 30, minY: 20, maxY: 30 };
-  //write test doc
-  const docId = await testEnv.withSecurityRulesDisabled(
-    addDoc(locationsCol, location)
-  );
+const addTestData = async ({ testEnv, collectionName, testData }) => {
+  let docId;
+  await testEnv.withSecurityRulesDisabled(async (e) => {
+    const db = await e.firestore();
+    const dbCollection = collection(db, collectionName);
+    docId = await (await addDoc(dbCollection, testData)).id;
+  });
   return docId;
 };
+
+const addTestLocation = async (testEnv) =>
+  await addTestData({
+    testEnv,
+    collectionName: "characterLocations",
+    testData: { name: "waldo", minX: 20, maxX: 30, minY: 20, maxY: 30 },
+  });
+
+const addTestRankings = async (testEnv) =>
+  await addTestData({
+    testEnv,
+    collectionName: "rankings",
+    testData: { name: "branden", time: 40 },
+  });
 
 const getRankings = async (db) => {
   const rankingsCollection = collection(db, "rankings");
@@ -54,13 +67,8 @@ const getRankings = async (db) => {
 };
 
 const addRanking = async ({ name, time }, db) => {
-  try {
-    const rankingsCollection = collection(db, "rankings");
-    const docRef = await addDoc(rankingsCollection, { name, time });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+  const rankingsCollection = collection(db, "rankings");
+  const docRef = await addDoc(rankingsCollection, { name, time });
 };
 
 const getCharacterLocations = async (db) => {
@@ -93,22 +101,102 @@ describe("characterLocations Tests", () => {
     const testEnv = await getTestEnv();
     const db = testEnv.unauthenticatedContext().firestore();
     const docId = await addTestLocation(testEnv);
+    const doc = db.collection("characterLocations").doc(docId);
 
-    await assertFails(db.collection("characterLocations").doc(docId).delete);
-  });
-
-  it("cannot delete collection", async () => {
-    const testEnv = await getTestEnv();
-    const db = testEnv.unauthenticatedContext().firestore();
-    const docId = await addTestLocation(testEnv);
-
-    await assertFails(deleteDoc(doc(db, "characterLocations")));
+    await assertFails(doc.delete());
   });
 
   it("cannot update docs", async () => {
     const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
     const docId = await addTestLocation(testEnv);
+    const doc = db.collection("characterLocations").doc(docId);
 
-    await assertFails(updateDoc(docId, { name: "odlaw" }));
+    await assertFails(doc.update({ name: "odlaw" }));
+  });
+});
+
+describe("basic rankings Tests", () => {
+  it("can read in list mode", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertSucceeds(getRankings(db));
+  });
+
+  it("can create docs", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertSucceeds(addRanking({ name: "matt", time: 40 }, db));
+  });
+
+  it("cannot read single docs", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+    const docId = await addTestRankings(testEnv);
+
+    await assertFails(db.collection("rankings").doc(docId).get());
+  });
+
+  it("cannot delete single docs", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+    const docId = await addTestRankings(testEnv);
+    const doc = db.collection("rankings").doc(docId);
+
+    await assertFails(doc.delete());
+  });
+
+  it("cannot update docs", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+    const docId = await addTestRankings(testEnv);
+    const doc = db.collection("rankings").doc(docId);
+
+    await assertFails(doc.update({ name: "odlaw" }));
+  });
+});
+
+describe("create rankings Tests", () => {
+  it("cannot create with time less than one", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertFails(addRanking({ name: "matt", time: 0 }, db));
+    await assertFails(addRanking({ name: "matt", time: -1 }, db));
+  });
+  it("cannot create if name blank", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertFails(addRanking({ name: "", time: 40 }, db));
+  });
+  it("cannot create if name has space", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertFails(addRanking({ name: " matt", time: 40 }, db));
+    await assertFails(addRanking({ name: " m att", time: 40 }, db));
+  });
+  it("cannot create if name has cursing", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertFails(addRanking({ name: "ass", time: 40 }, db));
+    await assertFails(addRanking({ name: "asshole", time: 40 }, db));
+  });
+  it("cannot create if name has >20 chars", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertFails(addRanking({ name: "maaaaaaaaaaaaaaaaaaaaaatt", time: 40 }, db));
+  });
+  it("cannot create if name is other than letters", async () => {
+    const testEnv = await getTestEnv();
+    const db = testEnv.unauthenticatedContext().firestore();
+
+    await assertFails(addRanking({ name: "1matt", time: 40 }, db));
+    await assertFails(addRanking({ name: "1m*att", time: 40 }, db));
   });
 });
